07c6ed85791b932eaf2ec88446a5e28a9d8dffd3
liangsongjie <463827694@qq.com>
2018-01-19 10:32:07 +0800

    1. 初始化项目

---
+++ SJNetWorking/SJNetWorking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJNetWorking
+//
+//  Created by Hello on 2018/1/19.
+//  Copyright © 2018年 Hello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SJNetWorking/SJNetWorking/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SJNetWorking
+//
+//  Created by Hello on 2018/1/19.
+//  Copyright © 2018年 Hello. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
